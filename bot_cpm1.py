6fffb579ba81e9f90bad384e50eafbad2c729d2c
cpmilija2012-jpg <cpmilija2012@gmail.com>
2026-08-17 23:59:08 +0200

    Change bot token and owner ID
    Updated BOT_TOKEN and OWNER_ID for the bot configuration.

--- bot_cpm1.py
+++ bot_cpm1.py
@@ -45,8 +45,8 @@ from aiogram.types import (
 #  CONFIG
 # ============================================================
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8905746024:AAEu-MNApDTnIo1jG2Nx5Do-TBYIJbQvAcE")
-OWNER_ID  = 8003371335
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8656972990:AAHptOIiFijHXvmEhMglB8bUcJr0YQQ06Zs")
+OWNER_ID  = 8884756222
 
 RATE_LIMIT_ACTIONS = 10
 RATE_LIMIT_SECONDS = 60
