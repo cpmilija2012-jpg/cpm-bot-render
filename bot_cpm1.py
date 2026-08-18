5df863e6365f4e0248339e2cb893dfc2f51a4c80
cpmilija2012-jpg <cpmilija2012@gmail.com>
2026-08-18 12:18:58 +0200

    Change BOT_TOKEN to a new environment variable
    Updated the BOT_TOKEN to a new value.

--- bot_cpm1.py
+++ bot_cpm1.py
@@ -45,7 +45,7 @@ from aiogram.types import (
 #  CONFIG
 # ============================================================
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8656972990:AAHptOIiFijHXvmEhMglB8bUcJr0YQQ06Zs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8656972990:AAF77lkHzAz_mR-cg4lDOLIbx57OrAkl96Y")
 OWNER_ID  = 8884756222
 
 RATE_LIMIT_ACTIONS = 10
